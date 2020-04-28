86e71b06677ae388cd89259d95de301d7681a193
Ravinder Sirohi <Ravinder.Sirohi@hovergen.com>
2020-04-28 15:47:18 +0100

    React project template added.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h3>My List</h3>
+    </div>
+  );
+}
+
+export default App;
